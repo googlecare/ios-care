d36e89019af9ad75f572f987d1774d4bf85ff2c2
googlecare <gosoxohyeah@gmail.com>
2025-02-18 21:01:28 +0530

    Add files via upload

--- text.js
+++ text.js
@@ -1,5 +1,3 @@
-window.defaultNumber = '+1‪‪(855) 346-5251‬‬
-‬‬‬';
-window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $143.95, Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 ‪(855) 346-5251‬‬
- Talk to an Apple Representative.!';
+window.defaultNumber = '+1‪(805) 400-2540‬';
+window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1‪(805) 400-2540‬ to unlock it!';
 
